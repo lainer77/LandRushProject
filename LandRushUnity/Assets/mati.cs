89178efdb82b80108c3607bd846420c92166ce69
sgshul <37652279+sgshul@users.noreply.github.com>
2018-06-15 11:12:05 +0900

    핸드

---
+++ LandRushUnity/Assets/mati.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mati : MonoBehaviour
+{
+    private PSMeshRendererUpdater mesh;
+	// Use this for initialization
+	void Start ()
+	{
+	    mesh = transform.GetChild(0).GetComponent<PSMeshRendererUpdater>();
+        mesh.UpdateMeshEffect(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(10,20,0);
+	}
+}
